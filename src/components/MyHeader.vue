af2d74fa3f580bdbf3626a6b3ab4e017e35c7933
Mac <mac@MacBook-Air-Mac.local>
2024-08-30 12:20:47 +0300

    add lang

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,13 +1,30 @@
 <script setup>
+import { useI18n } from 'vue-i18n';
+const { t, locale } = useI18n();
 
 function reloadPage() {
   window.location.reload();
 }
+
+const switchLanguage = (event) => {
+  const selectedLocale = event.target.value;
+  locale.value = selectedLocale;
+  localStorage.setItem('lang', selectedLocale);
+  window.location.reload();
+};
 </script>
 
 <template>
   <header class="header">
     <div>
+      <div>
+        <select id="language-select"
+                class=""
+                @change="switchLanguage">
+          <option value="en" :selected="locale === 'en'">English</option>
+          <option value="ua" :selected="locale === 'ua'">Україньска</option>
+        </select>
+      </div>
       <a href="#" @click.prevent="reloadPage">
         <img src="/images/logo.svg" class="logo" alt="Vite logo"/>
       </a>
@@ -16,10 +33,10 @@ function reloadPage() {
       <div class="navbar-container">
         <ul class="navbar-menu">
           <li class="navbar-item">
-            <router-link to="/" class="navbar-link">Головна</router-link>
+            <router-link to="/" class="navbar-link">{{ $t('home') }}</router-link>
           </li>
           <li class="navbar-item">
-            <router-link to="/favorites" class="navbar-link">Вибране</router-link>
+            <router-link to="/favorites" class="navbar-link">{{ $t('favorites') }}</router-link>
           </li>
         </ul>
       </div>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import { createApp } from 'vue'
 import './style.css'
 import router from './routes';
+import { createI18n } from 'vue-i18n';
+import { languages } from "./i18n/index.js";
+import { defaultLocale } from "./i18n/index.js";
 import App from './App.vue'
+
+
+const messages = Object.assign(languages);
+
+
+const localeStorageLang = localStorage.getItem('lang')
+const i18n = createI18n({
+    legacy: false,
+    locale: localeStorageLang || defaultLocale,
+    fallbackLocale: 'en',
+    messages
+});
 
 const app = createApp(App);
 
 app.use(router);
+app.use(i18n);
 app.mount('#app');
